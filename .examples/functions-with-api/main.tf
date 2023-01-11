83a64ca2a242feb62164d6417a83e642c7712337
antoine-milochau-shift <48644548+amilochau@users.noreply.github.com>
2023-01-11 18:17:32 +0100

    Introduce examples

---
+++ .examples/functions-with-api/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.44"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
+
+provider "aws" {
+  profile = var.aws_provider_settings.profile
+  region  = var.aws_provider_settings.region
+
+  default_tags {
+    tags = {
+      organization = var.conventions.organization_name
+      application  = var.conventions.application_name
+      host         = var.conventions.host_name
+      creator      = "AMI"
+    }
+  }
+}
+
+module "checks" {
+  source      = "../../shared/checks"
+  conventions = var.conventions
+}
+
+module "functions_app" {
+  source      = "../../aws/functions-app"
+  conventions = var.conventions
+
+  lambda_settings = {
+    memory_size_mb       = 128
+    architecture         = "arm64"
+    runtime              = "nodejs18.x"
+    deployment_file_path = "data/app.zip"
+    handler              = "handler.get"
+    expose_apis          = true
+  }
+}
---
+++ .examples/functions-without-api/variables.tf
@@ -0,0 +1,22 @@
+variable "conventions" {
+  description = "Conventions to use"
+  type = object({
+    organization_name = string
+    application_name  = string
+    host_name         = string
+  })
+  default = {
+    organization_name = "mil"
+    application_name  = "emails"
+    host_name         = "default"
+  }
+}
+
+variable "aws_provider_settings" {
+  description = "Settings to configure the AWS provider"
+  type = object({
+    profile = optional(string, "default")
+    region  = optional(string, "eu-west-3")
+  })
+  default = {}
+}

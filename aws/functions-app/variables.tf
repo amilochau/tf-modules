2bb7413ae01493fe3d9104449833e42c59fe2fd7
antoine-milochau-shift <48644548+amilochau@users.noreply.github.com>
2023-01-12 22:39:20 +0100

    Fix constraints on conventions

--- aws/functions-app/variables.tf
+++ aws/functions-app/variables.tf
@@ -7,17 +7,17 @@ variable "conventions" {
   })
 
   validation {
-    condition     = length(var.conventions.organization_name) > 2 && length(var.conventions.organization_name) < 8 && can(regex("^[a-z]+$", var.conventions.organization_name))
+    condition     = length(var.conventions.organization_name) >= 2 && length(var.conventions.organization_name) <= 8 && can(regex("^[a-z]+$", var.conventions.organization_name))
     error_message = "Organization name must use between 2 and 8 characters, only lowercase letters"
   }
 
   validation {
-    condition     = length(var.conventions.application_name) > 2 && length(var.conventions.application_name) < 12 && can(regex("^[a-z]+$", var.conventions.organization_name))
+    condition     = length(var.conventions.application_name) >= 2 && length(var.conventions.application_name) <= 12 && can(regex("^[a-z]+$", var.conventions.organization_name))
     error_message = "Application name must use between 2 and 12 characters, only lowercase letters"
   }
 
   validation {
-    condition     = length(var.conventions.host_name) > 3 && length(var.conventions.host_name) < 8 && can(regex("^[a-z0-9]+$", var.conventions.organization_name))
+    condition     = length(var.conventions.host_name) >= 3 && length(var.conventions.host_name) <= 8 && can(regex("^[a-z0-9]+$", var.conventions.organization_name))
     error_message = "Host name must use between 2 and 8 characters, only lowercase letters and numbers"
   }
 }

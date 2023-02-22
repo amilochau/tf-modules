51d39c9f416c24ce75539acd371012a205eaaf12
antoine-milochau-shift <48644548+amilochau@users.noreply.github.com>
2023-02-22 18:44:03 +0100

    Minor improvement

--- aws/functions-app/outputs.tf
+++ aws/functions-app/outputs.tf
@@ -10,5 +10,5 @@ output "apigateway_invoke_domain" {
 
 output "apigateway_invoke_origin_path" {
   description = "Invoke URL of the default API Gateway stage"
-  value       = local.has_http_triggers ? "/${module.api_gateway_api[0].apigateway_stage_name}" : ""
+  value       = local.has_http_triggers && module.api_gateway_api[0].apigateway_stage_name != "$default" ? "/${module.api_gateway_api[0].apigateway_stage_name}" : ""
 }

93795110597b50681749a7704142b19286b2acd9
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2023-02-24 14:32:03 +0100

    Test error handling

--- aws/functions-app/cognito-clients/outputs.tf
+++ aws/functions-app/cognito-clients/outputs.tf
@@ -5,5 +5,5 @@ output "cognito_user_pool_id" {
 
 output "cognito_client_ids" {
   description = "Ids of the created Cognito clients"
-  value       = values(aws_cognito_user_pool_client.cognito_userpool_client)[*].id
+  value       = values(aws_cognito_user_pool_client.cognito_userpool_client2)[*].id
 }

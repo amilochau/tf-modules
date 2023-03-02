3823191dc490d040c102174a1b74ddb3027688b1
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2023-03-02 14:19:54 +0100

    Register GitHub as an OIDC identity provider

---
+++ .examples/github-identity-provider/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.50, < 5.0.0"
+    }
+    github = {
+      source = "integrations/github"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
+
+provider "aws" {
+  profile = var.aws_provider_settings.profile
+  region  = var.aws_provider_settings.region
+
+  default_tags {
+    tags = {
+      application = var.conventions.application_name
+      host        = var.conventions.host_name
+    }
+  }
+}
+
+provider "github" {
+  owner = var.github_provider_settings.owner
+}
+
+module "checks" {
+  source      = "../../shared/checks"
+  conventions = var.conventions
+}
+
+module "identity_provider" {
+  source      = "../../github/identity-provider"
+  conventions = var.conventions
+
+  account_name = "amilochau"
+}
---
+++ .examples/github-identity-provider/variables.tf
@@ -0,0 +1,20 @@
+variable "conventions" {
+  description = "Conventions to use"
+  type = object({
+    application_name = string
+    host_name        = string
+  })
+  default = {
+    application_name = "sample"
+    host_name        = "default"
+  }
+}
+
+variable "aws_provider_settings" {
+  description = "Settings to configure the AWS provider"
+  type = object({
+    profile = optional(string, "default")
+    region  = optional(string, "eu-west-3")
+  })
+  default = {}
+}

ff18b8439cc0558021568e87920e794e87773bc0
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2023-11-17 00:20:50 +0100

    Use AWS provider with a version compatible with 'al.2023' lambda runtime

--- aws/static-web-app/main.tf
+++ aws/static-web-app/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.25, < 6.0.0"
+      version = ">= 5.26, < 6.0.0"
       configuration_aliases = [
         aws.us-east-1,
         aws.no-tags

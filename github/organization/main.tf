09bbf5b361586f21b09d4f8b7df93d23b3bac511
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2024-01-20 11:52:05 +0100

    Add the `github/organization` module to manage GitHub organizations (#76)

---
+++ github/organization/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = ">= 5.43.0, < 6.0.0"
+    }
+  }
+
+  required_version = ">= 1.6.3, < 2.0.0"
+}
+
+resource "github_organization_settings" "organization_settings" {
+  name = var.github_organization_settings.name
+  billing_email = var.github_organization_settings.billing_email
+  blog = var.github_organization_settings.blog
+  default_repository_permission = "none"
+  dependabot_alerts_enabled_for_new_repositories = true
+  dependabot_security_updates_enabled_for_new_repositories = true
+  dependency_graph_enabled_for_new_repositories = true
+  location = var.github_organization_settings.location
+  has_organization_projects = false
+  has_repository_projects = false
+  members_can_create_repositories = false
+  members_can_create_public_repositories = false
+  members_can_create_private_repositories = false
+  members_can_create_internal_repositories = false
+  members_can_create_pages = false
+  members_can_create_public_pages = false
+  members_can_create_private_pages = false
+  members_can_fork_private_repositories = false
+
+  #company = "Test Company"
+  #blog = "https://example.com"
+  #email = "test@example.com"
+  #description = "Test Description"
+  #web_commit_signoff_required = true
+  #advanced_security_enabled_for_new_repositories = false
+  #secret_scanning_enabled_for_new_repositories = false
+  #secret_scanning_push_protection_enabled_for_new_repositories = false
+}
---
+++ github/organization/variables.tf
@@ -0,0 +1,34 @@
+variable "conventions" {
+  description = "Conventions to use"
+  type = object({
+    organization_name = string
+    application_name  = string
+    host_name         = string
+    temporary         = optional(bool, false)
+  })
+
+  validation {
+    condition     = length(var.conventions.organization_name) >= 3 && length(var.conventions.organization_name) <= 5 && can(regex("^[a-z]+$", var.conventions.organization_name))
+    error_message = "Organization name must use between 3 and 5 characters, only lowercase letters"
+  }
+
+  validation {
+    condition     = length(var.conventions.application_name) >= 2 && length(var.conventions.application_name) <= 12 && can(regex("^[a-z]+$", var.conventions.application_name))
+    error_message = "Application name must use between 2 and 12 characters, only lowercase letters"
+  }
+
+  validation {
+    condition     = length(var.conventions.host_name) >= 3 && length(var.conventions.host_name) <= 8 && can(regex("^[a-z0-9]+$", var.conventions.host_name))
+    error_message = "Host name must use between 2 and 8 characters, only lowercase letters and numbers"
+  }
+}
+
+variable "github_organization_settings" {
+  description = "GitHub organization settings"
+  type = object({
+    name = string
+    billing_email = string
+    blog = string
+    location = string
+  })
+}

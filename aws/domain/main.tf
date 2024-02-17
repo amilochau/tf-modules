ba82230c8680b4d9207a1b6b8e15a248ec5e5f57
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2024-02-17 22:09:52 +0100

    Update versions for Terraform engine and providers

--- aws/domain/main.tf
+++ aws/domain/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.26, < 6.0.0"
+      version = ">= 5.37, < 6.0.0"
     }
   }
 
-  required_version = ">= 1.6.3, < 2.0.0"
+  required_version = ">= 1.7.3, < 2.0.0"
 }
 
 resource "aws_route53_zone" "route53_zone" {
--- github/repository/main.tf
+++ github/repository/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = ">= 5.43.0, < 6.0.0"
+      version = ">= 6.0.0, < 7.0.0"
     }
   }
 
-  required_version = ">= 1.6.3, < 2.0.0"
+  required_version = ">= 1.7.3, < 2.0.0"
 }
 
 locals {

6ff02f3a5e8246cf82cc4ad8ecb4c66d921da93d
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2025-02-13 08:41:25 +0100

    Define shared CloudFront policies as part of landing zones

--- aws/landing-zones/account/main.tf
+++ aws/landing-zones/account/main.tf
@@ -25,3 +25,69 @@ resource "aws_ssoadmin_account_assignment" "account_assignment" {
   target_id   = aws_organizations_account.account.id
   target_type = "AWS_ACCOUNT"
 }
+
+resource "aws_cloudfront_origin_request_policy" "cloudfront_origin_request_policy_defaultapi" {
+  name = "DefaultForApi"
+  comment = "Policy to forward standard parameters in viewer requests ('Authorization' header, no cookie, all query strings)"
+
+  headers_config {
+    header_behavior = "whitelist"
+    headers {
+      items = ["Authorization", "Content-Type"]
+    }
+  }
+
+  cookies_config {
+    cookie_behavior = "none"
+  }
+
+  query_strings_config {
+    query_string_behavior = "all"
+  }
+}
+
+resource "aws_cloudfront_response_headers_policy" "cloudfront_response_headers_policy_defaultapi" {
+  name = "DefaultForApi"
+  comment = "Allows localhost for COST requests, including preflight requests, and adds security headers"
+
+  cors_config {
+    access_control_allow_credentials = true
+    origin_override = true
+
+    access_control_allow_headers {
+      items = ["Authorization", "Content-Type"]
+    }
+    access_control_allow_methods {
+      items = ["GET", "HEAD", "PUT", "POST", "PATCH", "DELETE", "OPTIONS"]
+    }
+    access_control_allow_origins {
+      items = ["http://localhost:3000"]
+    }
+    access_control_expose_headers {
+      items = ["*"]
+    }
+  }
+  
+  security_headers_config {
+    strict_transport_security {
+      access_control_max_age_sec = 31536000
+      override = true
+    }
+    xss_protection {
+      mode_block = true
+      protection = true
+      override = true
+    }
+    content_type_options {
+      override = true
+    }
+    referrer_policy {
+      referrer_policy = "strict-origin-when-cross-origin"
+      override = true
+    }
+    frame_options {
+      frame_option = "SAMEORIGIN"
+      override = true
+    }
+  }
+}
